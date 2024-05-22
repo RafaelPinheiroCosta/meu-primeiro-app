07dd55b5f09c233e5a18350c3fe9d822fb3aee83
Rafael Costa <rafaelcost@gmail.com>
2024-05-22 17:29:34 -0300

    3

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Cabecalho from './componentes/Cabecalho';
 
 function App() {
   return (
     <div className="App">
       <h1>rafael costa</h1>
-    teste teste
+      <Cabecalho/> 
+      <Cabecalho/>   
     </div>
   );
 }
