49ff5bf5fc77b969b665a8247c0876f976b875e9
Rafael Costa <rafaelcost@gmail.com>
2024-05-24 17:23:41 -0300

    8

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import Cabecalho from './componentes/Cabecalho';
+import Nav from './componentes/Nav';
 
 function App() {
   return (
     <div className="App">
       <h1>rafael pinheiro costa</h1>
       <Cabecalho/> 
-      <Cabecalho/>   
+      <Cabecalho/> 
+      <Nav/>   
+
     </div>
   );
 }
---
+++ src/componentes/Nav/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import './style.css';
+
+function Nav(){
+    return(
+        <div className="nav">
+            <h1>Navegador</h1>
+        </div>
+    );
+}
+export default Nav;
